97955b875ad54865723a8f1b17bd8f782d636bb3
Mohamed Adel <mohamed.adel@barqsystems.com>
2021-08-20 16:55:47 +0300

    Add TypeOrm initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
